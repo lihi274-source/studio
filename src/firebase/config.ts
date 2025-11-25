798a5aee65f0b0b847b555b730740bf4ce9c0658
Lidia Hidalgo Castro <lidiahidalgocastro@gmail.com>
2025-11-25 14:52:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-283475835-6102e",
+  "appId": "1:14320085666:web:daa9338953379fdad885fb",
+  "apiKey": "AIzaSyDngb_674Tn52e-5abDscgJ5Q_I4JDRowc",
+  "authDomain": "studio-283475835-6102e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "14320085666"
+};
