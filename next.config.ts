972e2f0042622067a1587c9eea77b828db08e55d
Lidia Hidalgo Castro <lidiahidalgocastro@gmail.com>
2025-11-25 15:27:57 +0000

    pon esta imagen en escapada azul a Valencia: https://cdn-imgix.headout.c

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn-imgix.headout.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
