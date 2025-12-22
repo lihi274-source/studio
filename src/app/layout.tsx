771626483ee0cbb9fa475c30cbb52488da6ce604
Lidia Hidalgo Castro <lidiahidalgocastro@gmail.com>
2025-12-22 09:09:37 +0000

    cambiame la tipografia de toda la pagina web, para los titulos prinicpal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
