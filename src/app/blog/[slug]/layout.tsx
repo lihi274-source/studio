de1092d76ca42f80f36ed9b19fcf1a2a2708bac4
Lidia Hidalgo Castro <lidiahidalgocastro@gmail.com>
2026-01-19 09:26:18 +0000

    puedes arreglar que cuando entre al blog, las pestañas de llegir més, qu

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -1,5 +1,3 @@
-import Footer from "@/components/layout/footer";
-import Header from "@/components/layout/header";
 
 export default function BlogPostLayout({
     children,
@@ -7,14 +5,8 @@ export default function BlogPostLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex flex-col min-h-screen bg-background">
-            <Header />
-            <main className="flex-grow">
-                {children}
-            </main>
-            <Footer />
-        </div>
+        <>
+            {children}
+        </>
     );
 }
-
-    
