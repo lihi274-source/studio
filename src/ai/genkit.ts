97ee0b93adb87de870e48a3677f2d687c44da7da
Lidia Hidalgo Castro <lidiahidalgocastro@gmail.com>
2026-01-19 09:53:07 +0000

    me sigue dando el mismo error, soluciona porfa

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,7 @@
-import { config } from 'dotenv';
-config();
-
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
   model: 'googleai/gemini-2.5-flash',
 });
